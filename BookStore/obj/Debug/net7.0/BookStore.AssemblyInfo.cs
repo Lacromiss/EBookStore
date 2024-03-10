f386a7be89223a1df73e10387596d4de92aa75c0
lacromiss <lacromis@gmail.com>
2024-03-10 16:46:51 +0400

    manage controller

--- BookStore/obj/Debug/net7.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net7.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56cd0843103ee95b9292de88bf73aadf7b5de57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6795a5480d7ec02b6156c0145b92be407aa15d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
